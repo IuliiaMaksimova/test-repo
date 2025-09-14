72ea0a964c35221842ebd065b21b3f9484ddbd2b
User <User@MacBook-Air-452.local>
2025-09-15 01:58:08 +0900

    Добавила Prettier, переписала POM и Test для Nodejs.org

--- Tests/nodejsHomePageTest.js
+++ Tests/nodejsHomePageTest.js
@@ -1,24 +1,37 @@
 import { Builder, until } from "selenium-webdriver";
 import { expect } from "chai";
+<<<<<<< HEAD
 import { captureOnFailure } from "../Utils/saveHelpers.js";
 import nodejsHomePage from "../Pages/nodejsHomePage.js";
 import nodejsDownloadPage from "../Pages/nodejsDownloadPage.js";
 import nodejsSupportPage from "../Pages/nodejsSupportPage.js";
+=======
+import GoogleHomePage from "../Pages/nodejsHomePage.js";
+import { captureOnFailure } from "../saveOnFailure.js";
+import nodejsHomePage from "../Pages/nodejsHomePage.js";
+>>>>>>> b573a02 (Добавила Prettier, переписала POM и Test для Nodejs.org)
 
 describe("Nodejs Home Page", function () {
   this.timeout(30000);
 
   let driver;
+<<<<<<< HEAD
   let nodeHome;
   let downloadPage;
   let supportPage;
   let baseNodePage;
+=======
+  let nodeHome;;
+>>>>>>> b573a02 (Добавила Prettier, переписала POM и Test для Nodejs.org)
 
   before(async function () {
     driver = await new Builder().forBrowser("chrome").build();
     nodeHome = new nodejsHomePage(driver);
+<<<<<<< HEAD
     downloadPage = new nodejsDownloadPage(driver);
     supportPage = new nodejsSupportPage(driver);
+=======
+>>>>>>> b573a02 (Добавила Prettier, переписала POM и Test для Nodejs.org)
     await nodeHome.open();
   });
 
@@ -27,12 +40,18 @@ describe("Nodejs Home Page", function () {
   });
 
   afterEach(async function () {
+<<<<<<< HEAD
     if (this.currentTest && this.currentTest.state === "failed") {
       const title = this.currentTest.fullTitle();
       const { screenshotPath, pagePath } = await captureOnFailure(
         driver,
         title,
       );
+=======
+    if (this.currentTest.state === "failed") {
+      const title = this.currentTest.fullTitle();
+      const { screenshotPath, pagePath } = await captureOnFailure(driver, title);
+>>>>>>> b573a02 (Добавила Prettier, переписала POM и Test для Nodejs.org)
       console.log("Saved failure artifacts:", screenshotPath, pagePath);
     }
   });
@@ -48,6 +67,7 @@ describe("Nodejs Home Page", function () {
 
     await nodeHome.clickGetNodeButton();
 
+<<<<<<< HEAD
     const isDownloadPageVisible = await downloadPage.isPageDisplayed();
     expect(isDownloadPageVisible).to.be.true;
 
@@ -67,5 +87,24 @@ describe("Nodejs Home Page", function () {
     await nodeHome.goBack();
 
     await driver.wait(until.elementLocated(nodeHome.mainText), 10000);
+=======
+    const currentUrl = await driver.getCurrentUrl();
+    expect(currentUrl).to.include("download");
+
+await nodeHome.goBack();
+
+await driver.wait(until.elementLocated(nodeHome.mainText), 5000);
+});
+
+  it("should click Get Support button", async function () {
+  const isVisible = await nodeHome.isGetSupportButtonDisplayed();
+  expect(isVisible).to.be.true;
+
+  await nodeHome.clickGetSupportButton();
+
+  const currentUrl = await driver.getCurrentUrl();
+  expect(currentUrl).to.include("eol");
+
+>>>>>>> b573a02 (Добавила Prettier, переписала POM и Test для Nodejs.org)
   });
 });
