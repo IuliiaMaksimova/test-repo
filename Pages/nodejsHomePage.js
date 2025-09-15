f1ed1baa59447e643985dd9570e2f20c2520385e
User <User@MacBook-Air-452.local>
2025-09-15 21:26:57 +0900

    New baseNodePage and new tests
    New baseNodePage and new tests#

--- Pages/nodejsHomePage.js
+++ Pages/nodejsHomePage.js
@@ -3,6 +3,7 @@ import NodejsBasePage from "./baseNodePage.js"; // если наследуешь
 
 export default class NodejsHomePage extends NodejsBasePage {
   constructor(driver) {
+<<<<<<< HEAD
     super(driver);
     this.mainText = By.css("h1");
     this.buttonsContainer = By.className("layouts-module__mzYk8q__homeLayout");
@@ -10,6 +11,15 @@ export default class NodejsHomePage extends NodejsBasePage {
       "index-module__chIz9G__button index-module__chIz9G__primary !block dark:!hidden"
     );
     this.getSupportButton = By.css("a[href='/en/eol']");
+=======
+    this.driver = driver;
+    this.mainText = By.className("special");
+    this.buttonsContainer = By.className("layouts-module__mzYk8q__homeLayout");
+    this.getNodeButton = By.className(
+      "index-module__chIz9G__button index-module__chIz9G__primary !block dark:!hidden",
+    );
+    this.getSupportButton = By.className("!text-xs");
+>>>>>>> da7b404 (New baseNodePage and new tests)
   }
 
   async open() {
@@ -22,11 +32,43 @@ export default class NodejsHomePage extends NodejsBasePage {
   }
 
   async isGetNodeButtonDisplayed() {
-    const container = await this.driver.findElement(this.buttonsContainer);
-    const button = await this.driver.findElement(this.getNodeButton);
+    const containers = await this.driver.findElements(this.buttonsContainer);
+    if (containers.length === 0) {
+      return false;
+    }
+
+    const buttons = await this.driver.findElements(this.getNodeButton);
+    return buttons.length > 0 && (await buttons[0].isDisplayed());
+  }
+
+  async clickGetNodeButton() {
+    const containers = await this.driver.findElements(this.buttonsContainer);
+    if (containers.length === 0) {
+      throw new Error("Buttons container not found");
+    }
+
+    const buttons = await this.driver.findElements(this.getNodeButton);
+    if (buttons.length === 0) {
+      throw new Error("Get Node.js button not found");
+    }
+
+    await buttons[0].click();
+  }
+
+  async goBack() {
+    await this.driver.navigate().back();
+  }
+
+  async isGetSupportButtonDisplayed() {
+    const button = await this.driver.wait(
+      until.elementLocated(this.getSupportButton),
+      10000,
+    );
+    await this.driver.wait(until.elementIsVisible(button), 5000);
     return button.isDisplayed();
   }
 
+<<<<<<< HEAD
   async clickGetNodeButton() {
     const container = await this.driver.findElement(this.buttonsContainer);
     const button = await this.driver.findElement(this.getNodeButton);
@@ -59,4 +101,11 @@ export default class NodejsHomePage extends NodejsBasePage {
       "URL did not change to eol"
     );
   }
+=======
+  async clickGetSupportButton() {
+    const buttons = await this.driver.findElements(this.getSupportButton);
+    if (buttons.length === 0) throw new Error("Get Support button not found");
+    await buttons[0].click();
+  }
+>>>>>>> da7b404 (New baseNodePage and new tests)
 }
--- Tests/nodejsHomePageTest.js
+++ Tests/nodejsHomePageTest.js
@@ -1,6 +1,7 @@
 import { Builder, until } from "selenium-webdriver";
 import { expect } from "chai";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { captureOnFailure } from "../Utils/saveHelpers.js";
 import nodejsHomePage from "../Pages/nodejsHomePage.js";
 import nodejsDownloadPage from "../Pages/nodejsDownloadPage.js";
@@ -10,28 +11,45 @@ import GoogleHomePage from "../Pages/nodejsHomePage.js";
 import { captureOnFailure } from "../saveOnFailure.js";
 import nodejsHomePage from "../Pages/nodejsHomePage.js";
 >>>>>>> b573a02 (Добавила Prettier, переписала POM и Test для Nodejs.org)
+=======
+import { captureOnFailure } from "../Utils/saveHelpers.js";
+import nodejsHomePage from "../Pages/nodejsHomePage.js";
+import nodejsDownloadPage from "../Pages/nodejsDownloadPage.js";
+import nodejsSupportPage from "../Pages/nodejsSupportPage.js";
+>>>>>>> da7b404 (New baseNodePage and new tests)
 
 describe("Nodejs Home Page", function () {
   this.timeout(30000);
 
   let driver;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> da7b404 (New baseNodePage and new tests)
   let nodeHome;
   let downloadPage;
   let supportPage;
   let baseNodePage;
+<<<<<<< HEAD
 =======
   let nodeHome;;
 >>>>>>> b573a02 (Добавила Prettier, переписала POM и Test для Nodejs.org)
+=======
+>>>>>>> da7b404 (New baseNodePage and new tests)
 
   before(async function () {
     driver = await new Builder().forBrowser("chrome").build();
     nodeHome = new nodejsHomePage(driver);
 <<<<<<< HEAD
+<<<<<<< HEAD
     downloadPage = new nodejsDownloadPage(driver);
     supportPage = new nodejsSupportPage(driver);
 =======
 >>>>>>> b573a02 (Добавила Prettier, переписала POM и Test для Nodejs.org)
+=======
+    downloadPage = new nodejsDownloadPage(driver);
+    supportPage = new nodejsSupportPage(driver);
+>>>>>>> da7b404 (New baseNodePage and new tests)
     await nodeHome.open();
   });
 
@@ -40,6 +58,7 @@ describe("Nodejs Home Page", function () {
   });
 
   afterEach(async function () {
+<<<<<<< HEAD
 <<<<<<< HEAD
     if (this.currentTest && this.currentTest.state === "failed") {
       const title = this.currentTest.fullTitle();
@@ -52,6 +71,14 @@ describe("Nodejs Home Page", function () {
       const title = this.currentTest.fullTitle();
       const { screenshotPath, pagePath } = await captureOnFailure(driver, title);
 >>>>>>> b573a02 (Добавила Prettier, переписала POM и Test для Nodejs.org)
+=======
+    if (this.currentTest && this.currentTest.state === "failed") {
+      const title = this.currentTest.fullTitle();
+      const { screenshotPath, pagePath } = await captureOnFailure(
+        driver,
+        title,
+      );
+>>>>>>> da7b404 (New baseNodePage and new tests)
       console.log("Saved failure artifacts:", screenshotPath, pagePath);
     }
   });
@@ -67,6 +94,7 @@ describe("Nodejs Home Page", function () {
 
     await nodeHome.clickGetNodeButton();
 
+<<<<<<< HEAD
 <<<<<<< HEAD
     const isDownloadPageVisible = await downloadPage.isPageDisplayed();
     expect(isDownloadPageVisible).to.be.true;
@@ -90,21 +118,30 @@ describe("Nodejs Home Page", function () {
 =======
     const currentUrl = await driver.getCurrentUrl();
     expect(currentUrl).to.include("download");
+=======
+    const isDownloadPageVisible = await downloadPage.isPageDisplayed();
+    expect(isDownloadPageVisible).to.be.true;
+>>>>>>> da7b404 (New baseNodePage and new tests)
 
-await nodeHome.goBack();
-
-await driver.wait(until.elementLocated(nodeHome.mainText), 5000);
-});
+    await downloadPage.goBack();
+  });
 
   it("should click Get Support button", async function () {
-  const isVisible = await nodeHome.isGetSupportButtonDisplayed();
-  expect(isVisible).to.be.true;
+    await driver.wait(until.elementLocated(nodeHome.mainText), 10000);
+    const isVisible = await nodeHome.isGetSupportButtonDisplayed();
+    expect(isVisible).to.be.true;
 
-  await nodeHome.clickGetSupportButton();
+    await nodeHome.clickGetSupportButton();
 
-  const currentUrl = await driver.getCurrentUrl();
-  expect(currentUrl).to.include("eol");
+    const isSupportPageVisible = await supportPage.isPageDisplayed();
+    expect(isSupportPageVisible).to.be.true;
 
+<<<<<<< HEAD
 >>>>>>> b573a02 (Добавила Prettier, переписала POM и Test для Nodejs.org)
+=======
+    await nodeHome.goBack();
+
+    await driver.wait(until.elementLocated(nodeHome.mainText), 10000);
+>>>>>>> da7b404 (New baseNodePage and new tests)
   });
 });
